ef2a33147d798e77fd2c1510af7a960abbd2c67a
LATIFUL FIKRI <latifulfikri192@gmail.com>
2020-05-21 23:36:00 +0700

    deploy in heroku

--- app.py
+++ app.py
@@ -22,18 +22,18 @@ def start():
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
                                 tw.post_tweet(message)
-                                tw.delete_dm(id)
+                                #delete dm
                             else:
                                 print("DM will be posted with media")
                                 print(dms[i]['shorted_media_url'])
                                 tw.post_tweet_with_media(message, dms[i]['media'],dms[i]['shorted_media_url'], dms[i]['type'])
-                                tw.delete_dm(id)
+                                #delete dm
                         else:
                             print("DM deleted because its empty..")
-                            tw.delete_dm(id)
+                            #delete dm
                     else:
                         print("DM will be deleted because does not contains keyword..")
-                        tw.delete_dm(id)
+                        #delete dm
 
             dms = list()
 
